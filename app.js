c1b5035695f94dc4f04f55f99670e9c8ce5045d0
vectorAz <1213109483@qq.com>
2019-03-19 11:50:42 +0800

    wx第一次提交

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+app.get('/', (request, response) => {
+console.log('服务器接收到请求了');
+})
+app.listen(3000, err => {
+if (!err) {
+console.log('服务器启动成功');
+} else {
+console.log(err);
+}
+})
