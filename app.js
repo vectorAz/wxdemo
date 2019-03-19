1aaf3b278709aa83f88cf4109f190d8b1dcfdd1b
vectorAz <1213109483@qq.com>
2019-03-19 15:36:34 +0800

    wx第三次提交 完成与wx服务器的连接 在分支wx上

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 const express = require('express');
+const sha1 = require('sha1')
 const app = express();
-app.get('/', (request, response) => {
-console.log('服务器接收到请求了');
-console.log(request.body);
+//中间件 接收所有请求 
+app.use((req, res) => {
+    console.log(req.query);
+    /*
+    { 
+    signature://微信加密前面 'c4da27e5ccdbe150f59e413f5ca3130627acfb2a',
+    echostr: '612966292251671551',//微信后台的随机字符串
+    timestamp: '1552980228',//发送请求的时间
+    nonce: '1439175072' }  //后台生成的随机数字
+  */ 
+    const { signature,echostr, timestamp, nonce } = req.query
+    const token="Tohka319";
+    const sortArr=[token, timestamp, nonce ].sort();
+    //sort() 方法用原地算法对数组的元素进行排序，
+    const sha1Arr=sha1(sortArr.join(''))
+    //join()方法将数组中所有元素连接为一个字符串。
+    if(req.method==='GET'){ //GET是wx服务器转发给开发者服务器的
+        if(sha1Arr===signature){
+            res.end(echostr)
+            //返回echostr给wx后台 wx才会承认
+        }else{
+            console.log('error');
+            
+        }
+    }
+
+
+
 })
 app.listen(3000, err => {
-if (!err) {
-console.log('服务器启动成功');
-} else {
-console.log(err);
-}
-})
+        if (!err) {
+            console.log('服务器启动成功');
+        } else {
+            console.log(err);
+        }
+    })
