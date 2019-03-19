6d81a0eab102572767d3be33715fa82ec04074ce
vectorAz <1213109483@qq.com>
2019-03-19 11:56:53 +0800

    wx第二次提交  在分支wx上

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 app.get('/', (request, response) => {
 console.log('服务器接收到请求了');
+console.log(request.body);
 })
 app.listen(3000, err => {
 if (!err) {
